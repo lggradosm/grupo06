1e5c3ead2946229556238dfeeee5c83e1248befb
lggradosm <gabrielgradosmachado@gmail.com>
2022-11-20 17:47:31 -0500

    quiz javascript

--- js/index.js
+++ js/index.js
@@ -4,30 +4,81 @@ const $ = (id) => document.getElementById(id);
 
 const $answers = $("answers");
 const $question = $("question");
+const $counter = $("counter");
+
 let selectedQuestion = questions[0];
 $question.innerHTML = `<p>${selectedQuestion.question}</p>`;
 let answersHtml = "";
-let $options = $("options");
-const clickHandler = (e) => {
-  console.log("hola");
-};
+
+let activeOptions = true;
+let activeTimer = true;
 
 selectedQuestion.answers.map((answer, index) => {
-  answersHtml += `<li id="options${
-    index + 1
-  }" class="options__item f-elements f-elements--center">
+  answersHtml += `<li id="options" class="options__item f-elements f-elements--center">
     <div class="options__indicator">${index + 1}</div>
     <p class="options__item-answer">${answer.option}</p>
   </li>`;
 });
+
 $answers.innerHTML = answersHtml;
-$options = document
-  .getElementById("answers")
-  .getElementsByClassName("options__item");
-console.log(options);
-$options.map((option, index) => {
-  console.log(index);
-});
-$options.addEventListener("click", (e) => clickHandler(e), false);
-// $answers.outerwiHTML = `<ul class="options f-elements f-elements--gap-20" id="answers">
-// ${answersHtml}</ul>`;
+const $options = document.querySelectorAll("#options");
+
+let count = 10;
+
+$counter.innerHTML = `${count}`;
+setInterval(() => {
+  if (count > 0 && activeTimer) {
+    count--;
+    console.log(count);
+    $counter.innerHTML = `${count}`;
+  } else {
+    disableButtons();
+    showSuccessfullAnswer();
+  }
+}, 1000);
+
+const addSuccessfullStyle = (index) => {
+  $options[index].classList.add("options__item--selected");
+  $options[index].classList.add("options__item--successfull");
+};
+const addWrongStyle = (index) => {
+  $options[index].classList.add("options__item--selected");
+  $options[index].classList.add("options__item--wrong");
+};
+
+const showSuccessfullAnswer = () => {
+  selectedQuestion.answers.map((answer, index) => {
+    if (answer.isCorrect) addSuccessfullStyle(index);
+  });
+};
+
+const showWrongAnswer = (index) => {
+  addWrongStyle(index);
+  showSuccessfullAnswer();
+};
+
+const disableButtons = () => (activeOptions = false);
+
+// HANDLER FUNCTIONS
+const clickHandler = (index) => {
+  if (activeOptions) {
+    activeTimer = false;
+    selectedQuestion.answers[index].isCorrect
+      ? addSuccessfullStyle(index)
+      : showWrongAnswer(index);
+    disableButtons();
+  }
+};
+
+const keydownHandler = (value) => {
+  if (value == 1 || value == 2 || value == 3 || value == 4)
+    clickHandler(value - 1);
+};
+
+// EVENTS
+
+document.addEventListener("keydown", (event) => keydownHandler(event.key));
+
+$options.forEach(($option, index) =>
+  $option.addEventListener("click", () => clickHandler(index))
+);
